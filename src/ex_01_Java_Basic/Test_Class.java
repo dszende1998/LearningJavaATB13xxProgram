01e429143b0b0712ae9833f6d70a6bd8f0de43fc
Dhananjay <dszende1998@gmail.com>
2025-09-08 22:04:00 +0530

    Second class ok

--- src/ex_01_Java_Basic/Test_Class.java
+++ src/ex_01_Java_Basic/Test_Class.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basic;
 
 public class Test_Class {
     public static void main(String[] args) {
-        System.out.println("Second class");
+        System.out.println("Second class ok");
     }
 
 }
