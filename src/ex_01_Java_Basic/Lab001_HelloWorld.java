6e779cddaa0abe68f3105b27b8429633e1ef8d93
Dhananjay <dszende1998@gmail.com>
2025-09-08 21:04:23 +0530

    First Committ

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, I am RUNNING JAVA Program");
+        System.out.println("Hi");
     }
 }
