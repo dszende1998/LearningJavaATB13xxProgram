68f29b99941fb054b32fea01e2770844d47a45cb
Dhananjay <dszende1998@gmail.com>
2025-09-16 22:31:25 +0530

    14 sept part 1

---
+++ src/ex_01_Java_Basic/PrintF.java
@@ -0,0 +1,28 @@
+package ex_01_Java_Basic;
+
+public class PrintF {
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.printf("%dx1=%d", table, table*1);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*2);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*3);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*4);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*5);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*6);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*7);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*8);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*9);
+        System.out.println();
+        System.out.printf("%dx2=%d", table, table*10);
+
+
+    }
+}
